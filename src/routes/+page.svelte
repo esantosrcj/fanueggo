2adfe91caa50181566c391659da6fe5928e4bd54
Eddie Santos <esantosrcj@gmail.com>
2023-03-28 00:09:01 -0500

    Add head description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Fanueggo home" />
+</svelte:head>
+
 <div class="p-8">
 	<h1>Welcome.</h1>
 </div>
--- src/routes/basketball/+page.svelte
+++ src/routes/basketball/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<svelte:head>
+	<title>Basketball</title>
+	<meta name="description" content="Basketball page" />
+</svelte:head>
+
 <div class="p-8">
 	<h1>Basketball.</h1>
+	<Button href="/basketball/games">Today Games</Button>
 </div>
